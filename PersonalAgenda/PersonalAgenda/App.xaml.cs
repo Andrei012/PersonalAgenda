f644bb436e08e866c8060503e0b4f79e223f044b
DENIS NORBERT ANDREI <andrei.denis164133@student.ubbcluj.ro>
2020-12-18 16:09:07 +0200

    Add project files.

---
+++ PersonalAgenda/PersonalAgenda/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using System.IO;
+using PersonalAgenda.Data;
+
+namespace PersonalAgenda
+{
+    public partial class App : Application
+    {
+        static AgendaDatabase database;
+
+        public static AgendaDatabase Database()
+        {
+            if (database == null)
+            {
+                database = new
+               AgendaDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "Notes.db3"));
+            }
+            return database;
+        }
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new AgendaPage());
+        }
+    }
+
+}
